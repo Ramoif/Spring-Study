d51f0054cb10f98a69447e8063f49a73df9cb45b
Ramoif <Ramoif>
2020-12-30 10:53:08 +0800

    commit 12.30v2

--- spring-demo02/src/main/java/com/ycsx/demo03_proxy/Host.java
+++
@@ -1,8 +0,0 @@
-package com.ycsx.demo03_proxy;
-
-public class Host implements Rent{
-    @Override
-    public void rent() {
-        System.out.println("房东出租房屋。");
-    }
-}
--- spring-demo02/src/main/java/com/ycsx/demo03_proxy/Rent.java
+++
@@ -1,5 +0,0 @@
-package com.ycsx.demo03_proxy;
-
-public interface Rent {
-    public void rent();
-}
